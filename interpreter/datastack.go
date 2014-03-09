b933ca3605f4be55030a8ca2acd61632a31ce40e
Howard Mao <zhehao.mao@gmail.com>
2014-03-09 13:06:59 -0700

    more datastack utility functions

--- interpreter/datastack.go
+++ interpreter/datastack.go
@@ -18,10 +18,33 @@ func (stack *Stack) PopExpr() (Sexp, error) {
 	return elem.(DataStackElem).expr, nil
 }
 
+func (stack *Stack) PopExpressions(n int) ([]Sexp, error) {
+	arr := make([]Sexp, 0)
+	for i := 0; i < n; i++ {
+		elem, err := stack.Pop()
+		if err != nil {
+			return nil, err
+		}
+		arr = append(arr, elem.(DataStackElem).expr)
+	}
+	ReverseArray(arr)
+	return arr, nil
+}
+
 func (stack *Stack) GetExpr(n int) (Sexp, error) {
 	elem, err := stack.Get(n)
 	if err != nil {
 		return nil, err
 	}
 	return elem.(DataStackElem).expr, nil
+}
+
+// reverse array in-place
+func ReverseArray(arr []Sexp) {
+	size := len(arr)
+	for i := 0; i < size / 2; i++ {
+		temp := arr[i]
+		arr[i] = arr[size - i - 1]
+		arr[size - i - 1] = temp
+	}
 }
