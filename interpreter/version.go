df89c0c71a0791bdefe5f8d0de7307a14e2ab8e8
Howard Mao <zhehao.mao@gmail.com>
2014-03-09 21:07:13 -0700

    make it repl-y

---
+++ interpreter/version.go
@@ -0,0 +1,5 @@
+package glisp
+
+func Version() string {
+	return "0.0.1"
+}
--- main.go
+++ main.go
@@ -3,30 +3,99 @@ package main
 import (
 	"fmt"
 	"os"
+	"bufio"
 	"./interpreter"
 )
 
+func getLine(reader *bufio.Reader) (string, error) {
+	line := make([]byte, 0)
+	for {
+		linepart, hasMore, err := reader.ReadLine()
+		if err != nil {
+			return "", err
+		}
+		line = append(line, linepart...)
+		if !hasMore {
+			break
+		}
+	}
+	return string(line), nil
+}
+
+func isBalanced(str string) bool {
+	parens := 0
+	squares := 0
+
+	for _, c := range str {
+		switch c {
+		case '(':
+			parens++
+		case ')':
+			parens--
+		case '[':
+			squares++
+		case ']':
+			squares--
+		}
+	}
+
+	return parens == 0 && squares == 0
+}
+
+func getExpression(reader *bufio.Reader) (string, error) {
+	fmt.Printf("> ")
+	line, err := getLine(reader)
+	if err != nil {
+		return "", err
+	}
+	for !isBalanced(line) {
+		fmt.Printf(">> ")
+		nextline, err := getLine(reader)
+		if err != nil {
+			return "", err
+		}
+		line += "\n" + nextline
+	}
+	return line, nil
+}
+
 func main() {
 	env := glisp.NewGlisp()
-	err := env.LoadFile(os.Stdin)
+	reader := bufio.NewReader(os.Stdin)
 
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(-1)
+	fmt.Printf("glisp version %s\n", glisp.Version())
+
+	for {
+		line, err := getExpression(reader)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(-1)
+		}
+
+		if line == "quit" {
+			break
+		}
+		if line == "dump" {
+			env.DumpEnvironment()
+			continue
+		}
+
+		err = env.LoadString(line)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(-1)
+		}
+		err = env.Run()
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(-1)
+		}
+		expr, err := env.PopResult()
+		if err != nil {
+			fmt.Println("nothing on the stack")
+			os.Exit(-1)
+		}
+		fmt.Println(expr.SexpString())
 	}
 
-	env.DumpEnvironment()
-
-	err = env.Run()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(-1)
-	}
-
-	expr, err := env.PopResult()
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(-1)
-	}
-	fmt.Println(expr.SexpString())
 }
