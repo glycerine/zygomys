63ae259e9e3a30e95ed3e90a3fb119337fd2e8f1
Howard Mao <zhehao.mao@gmail.com>
2014-03-23 20:25:41 -0700

    testall.sh should print which test fails on failure

--- tests/testall.sh
+++ tests/testall.sh
@@ -2,5 +2,6 @@
 
 for lispfile in tests/*.glisp
 do
-    ./glisp "${lispfile}" && echo "${lispfile}: All tests passed"
+    ./glisp "${lispfile}" && echo "${lispfile}: All tests passed" || \
+        echo "test failure in ${lispfile}"
 done
