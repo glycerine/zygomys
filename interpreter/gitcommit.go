76854d4708578555df84a53bcf7539c1d270a142
Jason E. Aten <j.e.aten@gmail.com>
2016-01-04 06:21:36 -0800

    rename to jsonmsgp.go

--- interpreter/gitcommit.go
+++ interpreter/gitcommit.go
@@ -1,2 +1,2 @@
 package glisp
-func init() { GITLASTTAG = "v0.9.0"; GITLASTCOMMIT = "b70592f4aa4d9202fe064cbc0fe66c00631ac3df" }
+func init() { GITLASTTAG = "v0.9.0"; GITLASTCOMMIT = "8efcbb429665338cf8d7054eaefa7b3d56e39ddb" }
