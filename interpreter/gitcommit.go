87fe483e5f6decb309230dd7dd9647b141998634
jaten <jaten@betable.com>
2016-01-04 20:54:44 -0800

    v0.9.3 in place

--- interpreter/gitcommit.go
+++ interpreter/gitcommit.go
@@ -1,2 +1,2 @@
 package glisp
-func init() { GITLASTTAG = "v0.9.1"; GITLASTCOMMIT = "21ddc3663a6e8b774f714734188bb301a3a21992" }
+func init() { GITLASTTAG = "v0.9.3"; GITLASTCOMMIT = "9f07fa2e852a2c8488ee68033b63db4ce38f50f3" }
