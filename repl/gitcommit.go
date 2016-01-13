c48bf85b98a8b67e7e87f46215d62d5d6ff4053f
Jason E. Aten <j.e.aten@gmail.com>
2016-01-13 15:18:38 -0800

    update version hash

--- repl/gitcommit.go
+++ repl/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "v1.3.9"; GITLASTCOMMIT = "dd36632cb950eabe85d65083307cdb4fa3254291" }
+func init() { GITLASTTAG = "v1.3.9"; GITLASTCOMMIT = "5cdb6646e141eff829ca397c8833c3763dab31b9" }
