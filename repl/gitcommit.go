ba3d457d17e1de9ab943881b5ccbb0b2c78fe531
Jason E. Aten <j.e.aten@gmail.com>
2016-01-13 01:58:45 -0800

    atg. backout SimpleSource

--- repl/gitcommit.go
+++ repl/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "v1.3.0"; GITLASTCOMMIT = "65364eebb3ca389109eb529133dce6c158c0a7d7" }
+func init() { GITLASTTAG = "v1.3.0"; GITLASTCOMMIT = "d2274668ed8aefd010340e321b567006036e1a91" }
