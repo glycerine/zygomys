1289d557899e50d167e9f75d63271b646b1e8197
Jason E. Aten <j.e.aten@gmail.com>
2016-01-14 09:03:33 -0800

    v1.4.1 for loops scope

--- repl/gitcommit.go
+++ repl/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "v1.3.9"; GITLASTCOMMIT = "4f061c141ce98c3a2e258a7f8fd8f110ba61edce" }
+func init() { GITLASTTAG = "v1.4.1"; GITLASTCOMMIT = "7d786fed15b4acd50c1423ae2ea3f7f0dab96cca" }
