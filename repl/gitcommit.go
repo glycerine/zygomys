681083ad9c631480645fd5b75b34543116185e06
Jason E. Aten <j.e.aten@gmail.com>
2016-01-16 01:34:53 -0800

    tag 1.4.7 go calls green

--- repl/gitcommit.go
+++ repl/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "v1.4.5"; GITLASTCOMMIT = "b1dd0319ddf484c53eda7e539b33f585a4678457" }
+func init() { GITLASTTAG = "v1.4.7"; GITLASTCOMMIT = "bac1d522487f45256875c305e9aa0ffbb070f1da" }
