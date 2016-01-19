43b311fc60a755d6f1503a81368ab2289e7b3fe5
Jason E. Aten <j.e.aten@gmail.com>
2016-01-19 00:55:44 -0800

    v1.5.3 correct input continuation.

--- repl/gitcommit.go
+++ repl/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "v1.5.1"; GITLASTCOMMIT = "567aa375d1f0c4cbb0ec3994f6df4b93235a4879" }
+func init() { GITLASTTAG = "v1.5.3"; GITLASTCOMMIT = "ec0e31304ec5c6b8e14d6a7cee286cf084aea194" }
