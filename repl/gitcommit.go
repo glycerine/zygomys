02bbde34571029d601f6e36a29c99b032870400d
Jason E. Aten <j.e.aten@gmail.com>
2016-01-19 00:54:55 -0800

    atg. include parser.go.goroutine for future use.

--- repl/gitcommit.go
+++ repl/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "v1.5.1"; GITLASTCOMMIT = "c59aed5ed64f70eae42a09ee984adfe3d23593e5" }
+func init() { GITLASTTAG = "v1.5.1"; GITLASTCOMMIT = "567aa375d1f0c4cbb0ec3994f6df4b93235a4879" }
