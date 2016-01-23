82451b276f5e804f1fcdd808e4f9151f62d916bc
Jason E. Aten <j.e.aten@gmail.com>
2016-01-22 22:09:32 -0800

    tag 1.6.3 dot-symbols for OO notation.

--- repl/gitcommit.go
+++ repl/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "v1.6.2"; GITLASTCOMMIT = "5ea04218f65fbbf613ac3687804a84bf6d6b13ae" }
+func init() { GITLASTTAG = "1.6.3"; GITLASTCOMMIT = "45cef37bce111669da553304e53afbf994e06ff2" }
