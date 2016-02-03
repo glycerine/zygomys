a30c01eea65c3db25175e760b98f62b568a31f39
Jason E. Aten <j.e.aten@gmail.com>
2016-02-03 14:57:34 -0800

    split function, squashed. Fixes #7.

--- repl/gitcommit.go
+++ repl/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "v2.0.7"; GITLASTCOMMIT = "bb15843ab5c888157916bef9c195e8d12317cb80" }
+func init() { GITLASTTAG = "v2.0.7"; GITLASTCOMMIT = "970abcbfbbd00d9e0b1335e40d6c013e59af8405" }
