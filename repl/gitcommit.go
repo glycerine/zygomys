b53a26fbbdbe049cfac7a13505765b74e2550a21
Jason E. Aten <j.e.aten@gmail.com>
2016-02-15 01:27:34 -0800

    atg. noisy, about to quiet prints

--- repl/gitcommit.go
+++ repl/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "v2.2.3"; GITLASTCOMMIT = "c1edcb64ffed6197681b7e31de3874989b010518" }
+func init() { GITLASTTAG = "v2.2.3"; GITLASTCOMMIT = "2efda3b1839f9e0f584044a648d38c19bb802693" }
