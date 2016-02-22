fce95aa298025df7a3721335fdbc06c579401907
Jason E. Aten <j.e.aten@gmail.com>
2016-02-22 01:59:40 -0800

    atg. fix typo

--- repl/gitcommit.go
+++ repl/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "v3.0.0"; GITLASTCOMMIT = "bffc77ccc7aa7d2a8559a507c7ea0a9c53b99cfa" }
+func init() { GITLASTTAG = "v3.0.0"; GITLASTCOMMIT = "a3cf62dfa7c3f6240e3ac3c963cb20fda0b4e7d5" }
