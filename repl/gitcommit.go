a3cf62dfa7c3f6240e3ac3c963cb20fda0b4e7d5
Jason E. Aten <j.e.aten@gmail.com>
2016-02-22 01:58:15 -0800

    v3.0.0 release with struct type checking

--- repl/gitcommit.go
+++ repl/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "v2.2.2"; GITLASTCOMMIT = "4d4fa3ffb7f5260613d2278901901e80dcf8833d" }
+func init() { GITLASTTAG = "v3.0.0"; GITLASTCOMMIT = "bffc77ccc7aa7d2a8559a507c7ea0a9c53b99cfa" }
