b96a7b1a7f5f1f9ba1a5f57b65ec4645e684dc24
Jason E. Aten <j.e.aten@gmail.com>
2016-03-14 13:51:04 -0700

    atg. benefit of less space sensitivity in new lexer rules

--- repl/gitcommit.go
+++ repl/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "v4.2"; GITLASTCOMMIT = "5de1427e6d10f6fe6da2127b1c16b6795f14677a" }
+func init() { GITLASTTAG = "v4.2"; GITLASTCOMMIT = "aebc251e508aaf6e8f0bacb1b8976843f0b6934c" }
