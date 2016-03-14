fa503765bc47fce3dcd62fc0f4bafc556a8bdd1e
Jason E. Aten <j.e.aten@gmail.com>
2016-03-14 10:13:05 -0700

    link to Go comment definition

--- repl/gitcommit.go
+++ repl/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "v4.0"; GITLASTCOMMIT = "2924d0de39dcf13401e9048ae956f36236e81088" }
+func init() { GITLASTTAG = "v4.0"; GITLASTCOMMIT = "01ca1478e707401070dedd5738ee450a3ac8ee7f" }
