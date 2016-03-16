c095416cbc46412e39a8533ba1be9fc4be2787a1
Jason E. Aten <j.e.aten@gmail.com>
2016-03-15 22:06:34 -0700

    v4.2.4 tag

--- repl/gitcommit.go
+++ repl/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "v4.2.3"; GITLASTCOMMIT = "56d1819f6defe8ed66a460dee6085da3e3de0c37" }
+func init() { GITLASTTAG = "v4.2.4"; GITLASTCOMMIT = "aa6d0c816c55d70baa80fcd340d988c9f3626f42" }
