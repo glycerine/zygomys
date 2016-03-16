0d819318590e910fa5ceffa8b5322b64322c5355
Jason E. Aten <j.e.aten@gmail.com>
2016-03-16 15:42:14 -0700

    atg. slides++

--- slides/main.go
+++ slides/main.go
@@ -4,12 +4,10 @@ The zygomys command line REPL is known as `zygo`.
 package main
 
 // START OMIT
-
 import (
 ...
 	zygo "github.com/glycerine/zygomys/repl"
 )
-
 func main() {
 	// (1) configure it
 	// See library configuration convention: https://github.com/glycerine/configs-in-golang
@@ -18,15 +16,15 @@ func main() {
 	// (2) register your Go structs; give them a nickname
 	// here we register snoopy as a handle to Go struct &Snoopy{}
 	zygo.GoStructRegistry.RegisterUserdef("snoopy",
-		&zygo.RegisteredType{GenDefMap: true, Factory: func(env *zygo.Glisp) (interface{}, error) {
-			return &Snoopy{}, nil
-		}}, true)
+		&zygo.RegisteredType{GenDefMap: true,
+			Factory: func(env *zygo.Glisp) (interface{}, error) {
+				return &Snoopy{}, nil
+			}}, true)
 	
 	// (3) run the zygo repl
 	// -- the library does all the heavy lifting.
 	zygo.ReplMain(cfg)
 }
-
 // END OMIT
 
 func usage(myflags *flag.FlagSet) {
