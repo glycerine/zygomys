866a81adb84ecea5e1b3b3f61809ed88af4d21e9
Jason E. Aten <j.e.aten@gmail.com>
2016-03-19 15:20:30 -0700

    v4.3.2 repl accepts infix syntax

--- repl/gitcommit.go
+++ repl/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "v4.3.1"; GITLASTCOMMIT = "c2a252ccc93060b5cd4dece484140679eb0a057c" }
+func init() { GITLASTTAG = "v4.3.2"; GITLASTCOMMIT = "059f88a30bbe6bc439603c1ae1033225fa850c9d" }
