409e18453437922a0df02beeb24a54fca7266be3
Jason E. Aten <j.e.aten@gmail.com>
2016-03-19 01:18:43 -0700

    v4.3.0 infix arrary [] indexing

--- repl/gitcommit.go
+++ repl/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "v4.2.4"; GITLASTCOMMIT = "ce30bf41769c8ecc262e64859e9e1fd76617a039" }
+func init() { GITLASTTAG = "v4.3.0"; GITLASTCOMMIT = "88c8f67dd3207a09fceaff98cc084e20ec27b76b" }
