4714c831d6202c9b18e1fbba88114d0182155a9a
Jason E. Aten <j.e.aten@gmail.com>
2016-03-20 19:30:54 -0700

    v4.4.0 dot.sym set green

--- repl/gitcommit.go
+++ repl/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "v4.3.6"; GITLASTCOMMIT = "9621235cbd57f071fd2e9a94e21469735a3618f9" }
+func init() { GITLASTTAG = "v4.4.0"; GITLASTCOMMIT = "b1d2fb770bd81347ec1828306218f026109bb101" }
