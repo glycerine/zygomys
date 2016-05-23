a97e433f2bcc9fcb56448f33edb8119c94aea6fb
Jason E. Aten <j.e.aten@gmail.com>
2016-05-23 11:23:12 -0700

    atg. update if.zy tests to reflect current status

--- repl/gitcommit.go
+++ repl/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "4.6.7"; GITLASTCOMMIT = "15d38f35f1a0da140e870e9654270c1080fc1592" }
+func init() { GITLASTTAG = "4.6.7"; GITLASTCOMMIT = "c881af12ccbe31ce1f520743d5ed473f8772d909" }
