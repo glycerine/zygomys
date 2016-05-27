fdb9976d417226f84f49078ef5a65b97847e7846
Jason E. Aten <j.e.aten@gmail.com>
2016-05-26 18:27:49 -0700

    v4.6.8 with if-then-else infix parsing

--- repl/gitcommit.go
+++ repl/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "4.6.7"; GITLASTCOMMIT = "fcd7fe00da30d74e67e094ab13064c47f8db02fc" }
+func init() { GITLASTTAG = "4.6.7"; GITLASTCOMMIT = "1f28172126b0172b30e9531b0e450ef40ef32648" }
