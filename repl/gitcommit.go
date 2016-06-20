765c23c79f3eb71f2888ede527f23ac68d2f295a
Jason E. Aten <j.e.aten@gmail.com>
2016-06-19 22:39:23 -0500

    atg. remove gunk from package.zy

--- repl/gitcommit.go
+++ repl/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "v4.8.0"; GITLASTCOMMIT = "0929b664b2fad7b55705032d9e31c546f198e698" }
+func init() { GITLASTTAG = "v4.8.0"; GITLASTCOMMIT = "19cc3ac0521a67894c01d6f8f0c569ddd5f1fc68" }
