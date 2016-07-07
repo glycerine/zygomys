a6cb8b242f1d5a4b36116d7ae27f878508a4b39a
Jason E. Aten <j.e.aten@gmail.com>
2016-07-07 01:26:50 -0500

    update doc.go structure

--- repl/doc.go
+++ repl/doc.go
@@ -1,7 +1,8 @@
-// This project does not use godoc. Instead there is extensive
-//  and detailed description of the language features maintained
-//  on the wiki. See the following link.
-//
-// https://github.com/glycerine/zygomys/wiki
-//
+/*
+ This project does not use godoc. Instead there is extensive
+  and detailed description of the language features maintained
+  on the wiki. See the following link.
+
+ https:github.com/glycerine/zygomys/wiki
+*/
 package repl
