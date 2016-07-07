1e42b5668cf796eca72b78c73e1d45a15bebd0a6
Jason E. Aten <j.e.aten@gmail.com>
2016-07-07 01:21:35 -0500

    atg. provide link to wiki in doc.go

---
+++ repl/doc.go
@@ -0,0 +1,7 @@
+// This project does not use godoc. Instead there is extensive
+//  and detailed description of the language features maintained
+//  on the wiki. See the following link.
+//
+// https://github.com/glycerine/zygomys/wiki
+//
+package repl
