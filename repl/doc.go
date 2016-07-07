fed505dd14cfd481d3ff8d63ffe4ec59f751be93
Jason E. Aten <j.e.aten@gmail.com>
2016-07-07 01:30:37 -0500

    fix doc.go

--- repl/doc.go
+++ repl/doc.go
@@ -3,6 +3,6 @@ This project does not use godoc. Instead there is extensive
 and detailed description of the language features maintained
 on the wiki. See the following link.
 
-https:github.com/glycerine/zygomys/wiki
+https://github.com/glycerine/zygomys/wiki
 */
 package zygo
