e004037d8173c2839591832264473d8c7e583bc3
Jason E. Aten <j.e.aten@gmail.com>
2016-07-06 21:52:11 -0500

    atg. update package test to mention import

--- repl/gitcommit.go
+++ repl/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "v4.8.1"; GITLASTCOMMIT = "da44e3f397ade6fa565816f9bcc33badd2dfac32" }
+func init() { GITLASTTAG = "v4.8.1"; GITLASTCOMMIT = "9f1953775d39c277a7bd0cad937ed866c3777ce2" }
