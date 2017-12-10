a11a53ae0667f0d324c101458453df170d606b76
Jason E. Aten <j.e.aten@gmail.com>
2017-12-10 04:04:42 -0600

    atg. fix package

--- repl/gitcommit.go
+++ repl/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "v4.8.3"; GITLASTCOMMIT = "970f2d30cb331095d34444b405cc0e2af467f298" }
+func init() { GITLASTTAG = "v4.8.3"; GITLASTCOMMIT = "30b09f757c14d200f0b7dd7198b91229cbd63d9c" }
--- repl/hashutils.go
+++ repl/hashutils.go
@@ -1,4 +1,4 @@
-package repl
+package zygo
 
 import (
 	"errors"
