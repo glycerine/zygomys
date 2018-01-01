5b513d98f08c2dde7b537f6d88cfaa40bf21fe6a
Jason E. Aten <j.e.aten@gmail.com>
2018-01-01 15:37:48 -0600

    v5.0.1 commit

--- zygo/gitcommit.go
+++ zygo/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "v5.0.0"; GITLASTCOMMIT = "66e46224a17fea32103f98dee6af5cba62e800d2" }
+func init() { GITLASTTAG = "v5.0.1"; GITLASTCOMMIT = "4b7185e74b4ee47179904fef5d24328c71229f88" }
