65e2020f80b392523459dd66e3fc0f364c91671d
Jason E. Aten <j.e.aten@gmail.com>
2018-11-15 08:46:44 -0600

    atg. zygo -i remains interactive after running a script. Fixes #33

--- zygo/gitcommit.go
+++ zygo/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "v5.0.2"; GITLASTCOMMIT = "69914e17a60e6ccd048db0cccea887c64f845613" }
+func init() { GITLASTTAG = "v5.0.3"; GITLASTCOMMIT = "22c88044c7c2a3f845b8bbd1df6d990e8ee4f29c" }
