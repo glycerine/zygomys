a1c1d8cf01527a61c9a6f41046c923ab4c2220fe
Jason E. Aten <j.e.aten@gmail.com>
2022-08-03 18:27:16 -0500

    fix #54

--- zygo/gitcommit.go
+++ zygo/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "v6.0.0"; GITLASTCOMMIT = "1a199dd8e27c40a07b336c228e7d12b0582f0c7f" }
+func init() { GITLASTTAG = "v6.0.1"; GITLASTCOMMIT = "19c0bdf4c230eb298cfbeda7289c222783400e4b" }
