ad67040eddcbe92b597721dd60fcc7a622df788a
Jason E. Aten <j.e.aten@gmail.com>
2022-08-10 16:46:08 -0500

    update to v6.0.3

--- zygo/gitcommit.go
+++ zygo/gitcommit.go
@@ -1,2 +1,2 @@
 package zygo
-func init() { GITLASTTAG = "v6.0.2"; GITLASTCOMMIT = "800e300da75f558be2b62536e27bc1207d93cb76" }
+func init() { GITLASTTAG = "v6.0.2"; GITLASTCOMMIT = "7e6424f95eab93315ce9a5cc01829c642bb238fa" }
